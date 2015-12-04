d68edba86691bfd67f08942f511773bc50bad9f7
fengyanzhao <fyzh84@126.com>
2015-12-04 10:29:36 +0800

    chmod -R

--- android/btCall.sh
+++ android/btCall.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
-cnt=${1:-10000}
 am start -n com.android.phone/com.android.phone.DialMainActivity --es tag 0;
-for index in `seq $cnt`
+needPhoneOn = 0
+
+for para in $@
 do
+case $para in
+-o)
+needPhoneOn=1
+;;
+*)
+esac
+done
+
+for index in `seq 10000`
+do
+
 monkey -f monkey_10086 -v 1;
+if [ needPhoneOn -eq 0 ]
+then
 monkey -f monkey_bt_call -v 1;
+else
+monkey -f monkey_bt_call_phone_on -v 1;
+fi
+
 done
